8ce314132f48a4dae215632e53918e0d80e07084
gsh <gongshuhan1203@gmail.com>
2026-05-14 14:11:05 +0800

    完善登陆注册js功能

--- 哈理工计算机官网/js/login.js
+++ 哈理工计算机官网/js/login.js
@@ -1,17 +1,25 @@
-let btn = document.getElementById('btn')
+const btn = document.getElementById('btn');
 
 btn.onclick = () => {
-    let name = document.getElementById('name').value
-    let pwd = document.getElementById('pwd').value
-    let name1 = localStorage.getItem('lname')
-    let pwd1 = localStorage.getItem('lpwd')
-    if (name == name1 && pwd == pwd1) {
-        alert("登陆成功")
-        window.location.href = "./index.html";
-    } else if (name == name1 && pwd != pwd1) {
-        alert('密码错误！')
-    } else if (name != name1) {
-        alert('未注册！')
-        window.location.href = "./register.html"
+    const name = document.getElementById('name').value;
+    const pwd = document.getElementById('pwd').value;
+    const users = JSON.parse(localStorage.getItem('userlist'));
+    let flag = false;
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        if (user.lname === name) {
+            flag = true;
+            if (user.lpwd === pwd) {
+                alert("登录成功");
+                console.log(`${user.lname}登陆成功`);
+                window.location.href = "./index.html";
+            } else {
+                alert("密码错误！");
+            }
+        }
+    }
+    if (!flag) {
+        alert("未注册！");
+        window.location.href = "./register.html";
     }
 }
--- 哈理工计算机官网/js/register.js
+++ 哈理工计算机官网/js/register.js
@@ -1,10 +1,32 @@
-let btn = document.getElementById('btn')
+const btn = document.getElementById('btn');
 
 btn.onclick = () => {
-    let name = document.getElementById('name').value
-    let pwd = document.getElementById('pwd').value
-    localStorage.setItem('lname', name)
-    localStorage.setItem('lpwd', pwd)
-    alert("注册成功，请登录")
-    window.location.href = "./login.html";
+    const name = document.getElementById('name').value;
+    const pwd = document.getElementById('pwd').value;
+    let user_info = localStorage.getItem('userlist');
+    let users;
+    if (user_info) {
+        users = JSON.parse(user_info);
+    } else {
+        users = [];
+    }
+    //创建新用户对象
+    const newUser = {
+        lname: name,
+        lpwd: pwd
+    };
+    let flag = true;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].lname === name) {
+            alert('用户名重复！请更换！');
+            flag = false;
+            break;
+        }
+    }
+    if (flag) {
+        users.push(newUser);
+        localStorage.setItem('userlist', JSON.stringify(users));
+        alert("注册成功");
+        window.location.href = "./login.html";
+    }
 }
